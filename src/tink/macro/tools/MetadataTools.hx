84cc19256c10be2eb9c9112f60e25c2078320b48
Jason O'Neil <jason.oneil@gmail.com>
2013-02-24 16:41:41 +0800

    Change Hash() to Map()

--- src/tink/macro/tools/MetadataTools.hx
+++ src/tink/macro/tools/MetadataTools.hx
@@ -6,7 +6,7 @@ class MetadataTools
 {	
 	static public function toHash(m:Metadata)
 	{
-		var ret = new Hash<Array<Array<Expr>>>();
+		var ret = new Map<String, Array<Array<Expr>>>();
 		for (meta in m)
 		{
 			if (!ret.exists(meta.name))
